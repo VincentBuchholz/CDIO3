e163676159d1702a8436621bcca66c43d70678a8
vincent <vincbuchholz@gmail.com>
2020-11-24 13:22:43 +0100

    Die test printer antal forskellige resultater ud.

--- CDIO3/src/main/java/Test/DieTest.java
+++ CDIO3/src/main/java/Test/DieTest.java
@@ -2,11 +2,20 @@ package Test;
 
 import CDIO3.Die;
 
+import java.sql.SQLOutput;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DieTest {
 
     @org.junit.jupiter.api.Test
+    int one = 0;
+    int two = 0;
+    int three = 0;
+    int four = 0;
+    int five = 0;
+    int six = 6;
+
     void roll() {
         for (int i = 0; i < 1000; i++)
         {
@@ -14,6 +23,34 @@ class DieTest {
         DieTest.roll();
         System.out.print(DieTest.getDie() + " ");
         assertTrue(1 <= DieTest.getDie() && DieTest.getDie() <= 6);
+
+
+        if (DieTest.getDie() == 1){
+            one++;
+        }
+        if (DieTest.getDie() == 2){
+                two++;
+        }
+        if (DieTest.getDie() == 3){
+                three++;
+        }
+        if (DieTest.getDie() == 4){
+                four++;
+        }
+        if (DieTest.getDie() == 5){
+                five++;
+        }
+        if (DieTest.getDie() == 6){
+                six++;
+        }
     }
+
+        System.out.println("");
+        System.out.println("One: " +one);
+        System.out.println("Two: " + two);
+        System.out.println("Three: " + three);
+        System.out.println("Four: " + four);
+        System.out.println("Five: " + five);
+        System.out.println("Six: " + six);
 }
 }
